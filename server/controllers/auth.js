e232afbd5b6012ce40df827ad8d72be9127be136
Daniel Brusky <dmbrusky@gmail.com>
2021-03-18 15:36:15 -0400

    activation path for hosted app

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -26,7 +26,7 @@ exports.signup = (req, res) => {
       subject: "ACCOUNT ACTIVATION LINK",
       html: `
                 <h1>Please use the following link to activate your account</h1>
-                <a href="http://localhost:3000/auth/activate/${token}">http://localhost:8000/auth/activate/${token}</a>
+                <a href="http://localhost:8000/auth/activate/${token}">http://localhost:8000/auth/activate/${token}</a>
                 <hr />
                 <p>This email may contain sensitive information</p>
                 <p>http://localhost:3000</p>
